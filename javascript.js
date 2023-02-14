3a45c44a956959d92038f962e11d723616efe96e
lizzzshan <odysuessrox@gmail.com>
2023-02-14 14:51:01 -0800

    implement playRound function that determines the winner of the game

--- javascript.js
+++ javascript.js
@@ -17,5 +17,26 @@ function playRound(playerSelection, computerSelection){
     if (player == computerSelection){
         return("It's a tie!");
     }
+    // Player win options
+    else if((player == "rock" && computerSelection =="scissors")
+        || (player =="scissors" && computerSelection == "paper")
+        || (player == "paper" && computerSelection =="rock")){
+            console.log("You win, ", player, " beats ", computerSelection, "!");
+            return("win");
+    }
+    else{
+        console.log("You lose, ", computerSelection, " beats ", player);
+        return("lose");
+    }
+}
+
+console.log(playRound("rocK", getComputerChoice(moves)));
+// Plays the game for 5 rounds and returns the score
+function game(){
+    let cpuScore = 0;
+    let playerScore = 0;
     
+    for (let i =0; i < 5; i++){
+        //TODO: implement game func here 
+    }
 }
